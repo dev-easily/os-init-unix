63464a824dfa71fc8547ac861d158a5e704f1245
tb <travisbikkle@proton.me>
2024-08-27 20:40:00 +0800

    update

---
+++ 4.proxy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# github 加速 
+# git@gitee.com:easy-win/gh-proxy.git
+# 192.168.0.160
+
+
+# docker 加速 https://github.com/For-Backup/CF-Workers-docker.io
+# yu@kde:~/Projects/gh-proxy$ sudo cat /etc/docker/daemon.json
+# {
+#   "registry-mirrors": ["https://dh.jiasu.in", "https://hub.dockerx.org"]
+# }
