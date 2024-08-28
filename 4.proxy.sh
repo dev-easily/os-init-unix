c8036f14e49fc1824b3fec7f1d553645c5e9fe76
tb <travisbikkle@proton.me>
2024-08-28 14:32:27 +0800

    udpate

--- 4.proxy.sh
+++ 4.proxy.sh
@@ -9,3 +9,9 @@
 # {
 #   "registry-mirrors": ["https://dh.jiasu.in", "https://hub.dockerx.org"]
 # }
+
+# docker 配合私有仓库，将一些常用镜像拉取到本地，push到自己私有仓库
+# 用哪个 linux 用户登录，就用哪个 push。如果你用了 sudo，就一直用 sudo。
+# sudo docker login --username=你的用户名 registry.cn-hangzhou.aliyuncs.com
+# sudo docker tag 镜像ID registry.cn-hangzhou.aliyuncs.com/你的命名空间/你的镜像仓库:版本号 
+# sudo docker push registry.cn-hangzhou.aliyuncs.com/你的命名空间/你的镜像仓库:版本号 
