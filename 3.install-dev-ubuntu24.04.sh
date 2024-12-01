860c8c5f219ffe7b7a09736837dd062e312a8cc2
travisbikkle <travisbikkle@proton.me>
2024-12-01 16:35:54 +0800

    add xremap

--- 3.install-dev-ubuntu24.04.sh
+++ 3.install-dev-ubuntu24.04.sh
@@ -7,3 +7,13 @@ Host github.com
   User git
   ProxyCommand connect -S 127.0.0.1:7897 %h %p
 EOF
+
+# xremap
+
+# gnome tweak and `extensions` app
+sudo apt install -y gnome-tweaks gnome-shell-extensions chrome-gnome-shell
+cargo install xremap --features gnome   # GNOME Wayland
+sudo gpasswd -a `whoami` input
+echo 'KERNEL=="uinput", GROUP="input", TAG+="uaccess"' | sudo tee /etc/udev/rules.d/input.rules
+cp ./xremap.config.yml ~/.config/xremap.yml
+nohup sudo $HOME/.cargo/bin/xremap $HOME/.config/xremap.yml &
