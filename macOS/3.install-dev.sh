750ac6034ff5e597e6d0d80643bd72ebd1186e26
travisbikkle <travisbikkle@proton.me>
2025-03-04 17:38:44 +0800

    add login item

--- macOS/3.install-dev.sh
+++ macOS/3.install-dev.sh
@@ -34,33 +34,41 @@ EOF
 chmod +x ~/dev/open_terminal.sh
 
 # nvim, need python 3.12
-brew install neovim
-mkdir -p ~/.config/nvim
-git clone --depth=1 git@github.com:travisbikkle/nvim-config.git ~/.config/nvim
-# brew install --cask font-hack-nerd-font # 可以解压本仓库的字体
-brew install ripgrep
-/usr/local/bin/python3 -m venv ~/.config/nvim_python
-source ~/.config/nvim_python/bin/activate
-pip install -U pynvim
-pip install 'python-lsp-server[all]' pylsp-mypy python-lsp-isort python-lsp-black
-npm install -g vim-language-server
-brew install universal-ctags
-cargo component add rust-analyzer
+function install_nvim() {
+  brew install neovim
+  mkdir -p ~/.config/nvim
+  git clone --depth=1 git@github.com:travisbikkle/nvim-config.git ~/.config/nvim
+  # brew install --cask font-hack-nerd-font # 可以解压本仓库的字体
+  brew install ripgrep
+  /usr/local/bin/python3 -m venv ~/.config/nvim_python
+  source ~/.config/nvim_python/bin/activate
+  pip install -U pynvim
+  pip install 'python-lsp-server[all]' pylsp-mypy python-lsp-isort python-lsp-black
+  npm install -g vim-language-server
+  brew install universal-ctags
+  cargo component add rust-analyzer
+}
+install_nvim
 
 # emacs
-cargo component add rust-analyzer
-brew install ripgrep coreutils fd
-brew tap d12frosted/emacs-plus
-brew install emacs-plus
-ln -s /usr/local/opt/emacs-plus/Emacs.app /Applications/Emacs.app
-git clone --depth=1 git@github.com:doomemacs/doomemacs.git ~/.emacs.d
-DOOMGITCONFIG=~/.gitconfig ~/.emacs.d/bin/doom install
-rm -rf ~/.doom.d/
-git clone git@github.com:travisbikkle/.dooom.d.git ~/
-~/.emacs.d/bin/doom sync
-# M-x nerd-icons-install-fonts
-# M-x treesit-install-language-grammar typescript
-# M-x treesit-install-language-grammar rust
+function install_emacs() {
+  cargo component add rust-analyzer
+  brew install ripgrep coreutils fd
+  brew tap d12frosted/emacs-plus
+  brew install emacs-plus
+  ln -s /usr/local/opt/emacs-plus/Emacs.app /Applications/Emacs.app
+  git clone --depth=1 git@github.com:doomemacs/doomemacs.git ~/.emacs.d
+  DOOMGITCONFIG=~/.gitconfig ~/.emacs.d/bin/doom install
+  rm -rf ~/.doom.d/
+  git clone git@github.com:travisbikkle/.dooom.d.git ~/
+  ~/.emacs.d/bin/doom sync
+  # M-x nerd-icons-install-fonts
+  # M-x treesit-install-language-grammar typescript
+  # M-x treesit-install-language-grammar rust
+  sed "/emacs/d" -i ~/.zprofile
+  echo "/usr/local/bin/emacs --daemon" >> ~/.zprofile
+}
+install_emacs
 
 # git-quick-stats
 brew install coreutils
